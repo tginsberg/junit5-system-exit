951e9c351f8cacb92732e8ae332d4e687f3f3bad
Todd Ginsberg <todd@ginsberg.com>
2021-02-20 13:37:21 -0500

    GH-1 Make SystemExitPreventedException public

--- src/main/java/com/ginsberg/junit/exit/SystemExitPreventedException.java
+++ src/main/java/com/ginsberg/junit/exit/SystemExitPreventedException.java
@@ -28,6 +28,6 @@ package com.ginsberg.junit.exit;
  * A marker exception so we know that a System.exit was intercepted by our
  * SecurityManager.
  */
-class SystemExitPreventedException extends SecurityException {
+public class SystemExitPreventedException extends SecurityException {
 
 }
